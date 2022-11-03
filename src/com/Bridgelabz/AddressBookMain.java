a0cb1b492bf83a416e447f21ce9cc47fedeb2037
pratikraut25 <pratikraut2512@gmail.com>
2022-11-03 12:33:44 +0530

    creating-contact

--- src/com/Bridgelabz/AddressBookMain.java
+++ src/com/Bridgelabz/AddressBookMain.java
@@ -1,6 +1,6 @@
 package com.Bridgelabz;
 
-public class AddressBook {
+public class AddressBookMain {
 
 	public static void main(String[] args) {
     System.out.println("Welcome to Address Book Program");
